e673d8729b0b6e1d34f967a2a70c237810a12f39
Avalon <972003502@qq.com>
2019-05-17 00:50:45 +0800

    1
    1

--- miniprogram/pages/addProduct/addProduct.js
+++ miniprogram/pages/addProduct/addProduct.js
@@ -74,16 +74,9 @@ Page({
       sizeType: ['compressed'], //可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album'], //从相册选择
       success: (res) => {
-        let str = 'aaa.jpg';
-        str.slice(-3,0);
-        console.log(str);
-        if (res.tempFilePaths[0] == 'jpg') {
-          wx.navigateTo({
-            url: '../cropper/cropper?src=' + res.tempFilePaths[0]
-          })
-        } else {
-
-        }
+        wx.navigateTo({
+          url: '../cropper/cropper?src=' + res.tempFilePaths[0]
+        })
       },
       fail: e => { },
       complete: () => { }
