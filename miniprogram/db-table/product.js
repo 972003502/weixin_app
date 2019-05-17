49085eed7eeb8eadd96eb1f1ec94c2b4102fb3a9
Avalon <972003502@qq.com>
2019-05-17 21:41:04 +0800

    1
    1

---
+++ miniprogram/db-table/product.js
@@ -0,0 +1,11 @@
+let productTable = {
+  product_classify: null,
+  product_icon: null,
+  product_name: null,
+  product_describe: null,
+  product_price: null,
+  sales_status: null,
+  product_sort: null
+}
+
+export default productTable;
