edb862b9283eea7af4427473a44bbebae75e7693
Avalon <972003502@qq.com>
2019-05-20 09:51:28 +0800

    1
    1

--- miniprogram/pages/addProduct/addProduct.js
+++ miniprogram/pages/addProduct/addProduct.js
@@ -149,5 +149,59 @@ Page({
         });
       }
     })
+  },
+
+  onSubmit() {
+    this.cloudUploadFile();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.isFillComplete();
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+>>>>>>> Stashed changes
   }
 })
